fd068159786e53efd14424b33b105cd1bd3f04b9
Nitin Rao <sg727427@gmail.com>
2025-11-25 03:54:30 +0530

    Added production URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: 'http://localhost:5173', credentials: true}))
+app.use(cors({origin:[
+    "http://localhost:5173",
+    "https://role-based-auth-system-5efj.vercel.app"
+  ], credentials: true}))
 
 app.use(express.urlencoded({ extended: true}))
 
