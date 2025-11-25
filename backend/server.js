1bde095b038cb451e0a4373023cd033d87b4dacf
Nitin Rao <sg727427@gmail.com>
2025-11-25 17:29:28 +0530

    Updated CORS to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:[
+app.use(cors({
+  origin: [
     "http://localhost:5173",
     "https://role-based-auth-system-eight.vercel.app"
   ],
-  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+  credentials: true
+}));
+
 
 app.use(express.urlencoded({ extended: true}))
 
